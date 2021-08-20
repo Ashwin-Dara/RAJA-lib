45bfd42c5c2c03d44c12addbdb9cd9b35a7256e0
Ashwin D <niwhsa.d@gmail.com>
2021-08-20 09:40:07 -0700

    Added comments and refactored the header file for the Closed Loop Controller. Look at this file in the future for reference on the formats

--- include/RAJA/controllers/Closed-Loop-Controller.h
+++ include/RAJA/controllers/Closed-Loop-Controller.h
@@ -1,44 +1,130 @@
 #ifndef CLOSED_LOOP_H_
 #define CLOSED_LOOP_H_
-#include "main.h"
 #define INTERGAL_ERROR_TOLERANCE 3
 #define SETTLED_TOLERANCE 1.5f
+#define UPDATE_RATE 50
+
+#include "main.h"
+
+namespace RAJA{
+
+    class ClosedLoop{
+
+        protected: 
+            /* 
+            control_mode - 'L' indicates the controller will be used for linear movements. 
+            */
+            char control_mode; 
 
 
-class ClosedLoop{
-    protected: 
+            /*
+            * kP, kI, kD - constants used in the PID equation. 
+            * power - total power calculated by PID controller
+            * heading_error - difference between desired yaw and current yaw 
+            * heading_multiplier - correctional power constant (works as a P controller)
+            */
+            float kp, ki, kd, ls, power, heading_error, heading_multiplier; 
+            
 
-        char control_mode; 
-
-        float kp, ki, kd, ls, power, heading_error, heading_multiplier; 
-        
-        int integral, deriv, p_power, i_power, d_power, i_bound, accel_step; 
-
-        bool using_heading_correction, using_slew_rate, settled; 
+            /*
+            * These essentially are local variables for 'get_calculated_power' function. 
+            * The reason these are included as variables here so they can be accessed in different methods in the class.
+            */
+            int integral, deriv, p_power, i_power, d_power, i_bound, accel_step; 
 
 
-    public: 
-        static int sign(int x); 
+            /*
+            * using_heading_correction - true indicates we wish to apply a heading correction power
+            * using_slew_rate - true indicates we wish to 'slew' and cap the controller's acceleration
+            * settled - true indicates the robot has settled (the movement is complete or steady state error is reached)
+            */ 
+            bool using_heading_correction, using_slew_rate, settled; 
 
-        ClosedLoop(float kp, float ki, float kd, float ls, char state='L'); 
-        
-        void set_constants(float _kp, float _kd, float _ki, float _ls); 
 
-        void limit_acceleration(bool state, int max_accel); 
-        
-        float get_calculated_power(int set_point, float sensor_value);
-        
-        void set_heading_state(bool heading_state); 
+            /*
+            * Returns the sign of the input.
+            *
+            * @param {int} x - this is the number to obtain the sign of 
+            * @return {int} - sign of the number inputted. 
+            */
+            static int sign(int x); 
 
-        void set_heading_multiplier(float heading_multi); 
 
-        void set_heading_value(float error); 
+        public: 
 
-        bool get_heading_state() const; 
+            /*
+            * Constructor, initialize the PID constants and whether or not the PID is for linear movement. 
+            *
+            * @parameter {float} kp - kP constant in the PID equation
+            * @parameter {float} ki - kI constant in the PID equation
+            * @parameter {float} kd - kD constant in the PID equation
+            * @parameter {float} ls - max acceleration step of the controller 
+            * @parameter {char} state - 'L' indicates the PID is for linear movement. 'L' state enables heading correction.
+            */
+            ClosedLoop(float kp, float ki, float kd, float ls, char state='L'); 
 
-        char get_mode() const; 
 
-        bool has_settled() const; 
-};
+            /*
+            * Similar to constructor. Function will be used to reassign the constants.
+            */
+            void set_constants(float _kp, float _kd, float _ki, float _ls); 
+
+
+            /*
+            * Sets whether or not to enable slew control.  
+            *
+            * @param {bool} state - if true, slew control will be enabled 
+            * @param {int} max_accel - this is the max slew step for the PID controller
+            */
+            void limit_acceleration(bool state, int max_accel); 
+
+
+            /*
+            * Calculates the PID power. 
+            * 
+            * @param {int} set_point - this is the desired target for the PID controller
+            * @param {float} sensor_value - this is the current sensor value needed to be entered
+            * @return {float} - returns the calculated power of the PID controller
+            */
+            float get_calculated_power(int set_point, float sensor_value);
+
+
+            /* 
+            * Sets whether or not heading correction is enabled. 
+            */
+            void set_heading_state(bool heading_state); 
+
+
+            /* 
+            * Sets the heading correction error multiplier (p constant).
+            */
+            void set_heading_multiplier(float heading_multi); 
+
+
+            /*
+            * Sets the error of the heading. E.g., set_heading_value(90 - imu.get_rotation()) would set an error of 90 minus the current yaw. 
+            */
+            void set_heading_value(float error); 
+
+
+            /*
+            * Gets whether or not the heading correction is true. 
+            */
+            bool get_heading_state() const; 
+
+
+            /*
+            * Gets the mode of the controller. 'L' indicates that the control is linear. 
+            */
+            char get_mode() const; 
+
+
+            /*
+            * Gets whether or not the controller has settled (either reached target or encounters steady state error).
+            */
+            bool has_settled() const; 
+    };
+    
+}
 
 #endif
--- src/LIBRARY/Closed-Loop-Controller.cpp
+++ src/LIBRARY/Closed-Loop-Controller.cpp
@@ -2,77 +2,91 @@
 #include "../../include/RAJA/controllers/Closed-Loop-Controller.h"
 #include <iostream>
 
-ClosedLoop::ClosedLoop(float kp, float ki, float kd, float ls, char state):
-                       kp(kp), ki(ki), kd(kd), ls(ls), control_mode(state){}
+namespace RAJA{
+
+    // Initializing all of the PID constants in the constructor 
+    ClosedLoop::ClosedLoop(float kp, float ki, float kd, float ls, char state):
+                        kp(kp), ki(ki), kd(kd), ls(ls), control_mode(state){}
 
 
-void ClosedLoop::set_constants(float _kp, float _kd, float _ki, float _ls){
-    kp = _kp; 
-    ki = _ki; 
-    kd = _kd; 
-    ls = _ls; 
-}
-
-int ClosedLoop::sign(int x){
-    if(x>0) return 1; 
-    else return -1;
-}
-
-void ClosedLoop::limit_acceleration(bool state, int max_accel){
-    using_slew_rate = state; 
-    accel_step = max_accel;
-}
-
-float ClosedLoop::get_calculated_power(int set_point, float sensor_value){
-    int error = set_point - sensor_value; 
-    p_power = kp * error; 
-    pros::delay(50); 
-
-    if(abs(error) < i_bound && abs(error) > INTERGAL_ERROR_TOLERANCE){
-        integral += error; 
-    }
-    else{
-        integral = 0; 
-    }
-    i_power = ki * integral; 
-
-    int delta_error = (set_point - sensor_value) - error; 
-    d_power = kd * delta_error; 
-
-    if(abs(delta_error) < SETTLED_TOLERANCE){
-        settled = true; 
-    } 
-
-    if(using_slew_rate && abs(delta_error) > accel_step){
-        power += (sign(p_power) * ls); 
-    }
-    else{
-        power = p_power + i_power + d_power; 
+    // Another setter function for the main PID constants
+    void ClosedLoop::set_constants(float _kp, float _kd, float _ki, float _ls){
+        kp = _kp; 
+        ki = _ki; 
+        kd = _kd; 
+        ls = _ls; 
     }
 
-    return power; 
-}
+    int ClosedLoop::sign(int x){
+        if(x > 0){
+            return 1; 
+        } 
+        else{
+            return -1; 
+        } 
+    }
 
-void ClosedLoop::set_heading_state(bool heading_state){
-    using_heading_correction = heading_state; 
-}
+    void ClosedLoop::limit_acceleration(bool state, int max_accel){
+        using_slew_rate = state; 
+        accel_step = max_accel;
+    }
 
-void ClosedLoop::set_heading_multiplier(float heading_multi){
-    heading_multiplier = heading_multi; 
-}
+    float ClosedLoop::get_calculated_power(int set_point, float sensor_value){
+        int error = set_point - sensor_value; 
+        p_power = kp * error; 
+        pros::delay(UPDATE_RATE); 
 
-void ClosedLoop::set_heading_value(float error){
-    heading_error = error; 
-}
+        if(abs(error) < i_bound && abs(error) > INTERGAL_ERROR_TOLERANCE){
+            integral += error; 
+        }
+        else{
+            integral = 0; 
+        }
+        i_power = ki * integral; 
 
-bool ClosedLoop::get_heading_state() const{
-    return using_heading_correction; 
-}
+        int delta_error = (set_point - sensor_value) - error; 
+        d_power = kd * delta_error; 
 
-char ClosedLoop::get_mode() const{
-    return control_mode; 
-}
+        if(abs(delta_error) < SETTLED_TOLERANCE){
+            settled = true; 
+        } 
 
-bool ClosedLoop::has_settled() const{
-    return settled; 
+        if(using_slew_rate && abs(delta_error) > accel_step){
+            power += (sign(p_power) * ls); 
+        }
+        else{
+            power = p_power + i_power + d_power; 
+        }
+
+        return power; 
+    }
+
+    void ClosedLoop::set_heading_state(bool heading_state){
+        using_heading_correction = heading_state; 
+    }
+
+    void ClosedLoop::set_heading_multiplier(float heading_multi){
+        heading_multiplier = heading_multi; 
+    }
+
+    void ClosedLoop::set_heading_value(float error){
+        heading_error = error; 
+    }
+
+    bool ClosedLoop::get_heading_state() const{
+
+        if(control_mode == 'L'){
+            return true; 
+        }
+        return using_heading_correction; 
+    }
+
+    char ClosedLoop::get_mode() const{
+        return control_mode; 
+    }
+
+    bool ClosedLoop::has_settled() const{
+        return settled; 
+    }
+
 }
