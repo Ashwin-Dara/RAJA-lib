e98fbb5447f3eca5c467ec7a6c5f265151aeee66
Ashwin D <niwhsa.d@gmail.com>
2021-08-21 11:23:26 -0700

    Completed refactoring the Chassis-Object source file. Will be doing the Drive control file next.

--- src/LIBRARY/Chassis-Object.cpp
+++ src/LIBRARY/Chassis-Object.cpp
@@ -3,40 +3,55 @@
 
 namespace RAJA{
 
-std::shared_ptr<pros::Motor> rDrive1, rDrive2, lDrive1, lDrive2; 
+    /*
+    * Electronics that will be used internally by the ChassisObject class. 
+    */
+    std::shared_ptr<pros::Motor> rDrive1, rDrive2, lDrive1, lDrive2; 
 
-ChassisObject::ChassisObject(const pros::Motor &m1, const pros::Motor &m2, 
-                             const pros::Motor &m3, const pros::Motor &m4) {
+    ChassisObject::ChassisObject(const pros::Motor &m1, const pros::Motor &m2, 
+                                 const pros::Motor &m3, const pros::Motor &m4) {
 
-// Creating motor parameters: port, gearset, reverse, encoder_units
+    // Constructing the motors that will be use internally by the Chassis Object Class.
 
-rDrive1 = std::make_shared<pros::Motor>(m1.get_port(), m1.get_gearing(), m1.is_reversed(), m1.get_encoder_units());
-rDrive2 = std::make_shared<pros::Motor>(m2.get_port(), m2.get_gearing(), m2.is_reversed(), m2.get_encoder_units());
+    rDrive1 = std::make_shared<pros::Motor>(m1.get_port(), m1.get_gearing(), 
+                                            m1.is_reversed(), m1.get_encoder_units());
 
-lDrive1 = std::make_shared<pros::Motor>(m3.get_port(), m3.get_gearing(), m3.is_reversed(), m3.get_encoder_units());
-lDrive2 = std::make_shared<pros::Motor>(m4.get_port(), m4.get_gearing(), m4.is_reversed(), m4.get_encoder_units());
+    rDrive2 = std::make_shared<pros::Motor>(m2.get_port(), m2.get_gearing(), 
+                                            m2.is_reversed(), m2.get_encoder_units());
 
-}
+    lDrive1 = std::make_shared<pros::Motor>(m3.get_port(), m3.get_gearing(), 
+                                            m3.is_reversed(), m3.get_encoder_units());
 
-
-void ChassisObject::move_right_side (int voltage){
-    rDrive1->move(voltage); 
-    rDrive2->move(voltage);
-}
-
- void ChassisObject::move_left_side (int voltage){
-    lDrive1->move(voltage); 
-    lDrive2->move(voltage);
-}
-
-void ChassisObject::move(int voltage){
-    move_right_side(voltage); 
-    move_left_side(voltage);
-}
+    lDrive2 = std::make_shared<pros::Motor>(m4.get_port(), m4.get_gearing(), 
+                                            m4.is_reversed(), m4.get_encoder_units());
+    }
 
 
- void absolute_move_to(ChassisObject &drive, ClosedLoop &controller, float sensor_value, int set_point, int timeout){
-        int manual_timeout = 0; 
+    void ChassisObject::move_right_side (int voltage){
+        // Moving the right motors at the specified voltage.
+        rDrive1->move(voltage); 
+        rDrive2->move(voltage);
+    }
+
+
+    void ChassisObject::move_left_side (int voltage){
+        // Moving the left motors at the specified voltage.
+        lDrive1->move(voltage); 
+        lDrive2->move(voltage);
+    }
+
+
+    void ChassisObject::move(int voltage){
+        // Moving the drive at the inputted voltage. 
+        move_right_side(voltage); 
+        move_left_side(voltage);
+    }
+
+
+    void absolute_move_to(ChassisObject &drive, ClosedLoop &controller, float sensor_value, int set_point, int timeout){
+        // Passing in the chassis so that we can move it. Passing in the ClosedLoop so we know what controller to apply. 
+        int manual_timeout = 0; // Time override in the case that the drive does not "settle"
+
         while(!controller.has_settled() || (manual_timeout < timeout && manual_timeout > 0)){
 
             drive.move_left_side(controller.get_calculated_power(set_point, timeout));
@@ -47,20 +62,18 @@ void ChassisObject::move(int voltage){
         }
     }
 
+
     void relative_move_to(ChassisObject &drive, ClosedLoop &controller, float sensor_value, int set_point, int timeout){
+            // Using the absolute_move_to function but with modified sensor logic
         absolute_move_to(drive, controller, sensor_value, set_point - sensor_value, timeout);
     }
-// NEED TO PASS IN THE CHASSIS OBJECT THROUGH REFERENCE 
 
-/* SAMPLE USAGE 
+        /* SAMPLE USAGE 
 
-ChassisObject drive1(rightMotor, rightMotor2, leftMotor, leftMotor2); 
-ClosedLoop pid1(0.18, 0.01, 0.05, 2, 'T');
+        ChassisObject drive1(rightMotor, rightMotor2, leftMotor, leftMotor2); 
+        ClosedLoop pid1(0.18, 0.01, 0.05, 2, 'T');
 
-move_to_point(drive1, pid1, imu.get_position(), 90);
+        move_to_point(drive1, pid1, imu.get_position(), 90);
 
-
-*/
-
-
+        */
 }
