6abc4a2243b8e6f8f9e1f2ec6cd8b4f46068d2a5
Ashwin D <niwhsa.d@gmail.com>
2021-08-21 11:43:44 -0700

    Finished refactoring and commenting the Drive-Control header file.

--- include/RAJA/usercontrol/Drive-Opcontrol.h
+++ include/RAJA/usercontrol/Drive-Opcontrol.h
@@ -5,8 +5,23 @@
 #include "main.h"
 #include "../object/Chassis-Object.h"
 
-void tank_drive(pros::Controller &joystick, RAJA::ChassisObject &drive);
+namespace RAJA{
 
-void arcade_drive(pros::Controller &joystick, RAJA::ChassisObject &drive); 
+    /*
+    * Function that provides tank drive control to the inputted ChassisObject.
+    * 
+    * @param {pros::Controller} joystick - this is the joystick that the function will retrieve button states and channel inputs from.
+    * @param {RAJA::ChassisObject} drive - this is the ChassisObject that the function will control.
+    */
+    void tank_drive(pros::Controller &joystick, RAJA::ChassisObject &drive);
+
+    /*
+    * Function that provides 'arcade' control to the inputted ChassisObject.
+    * 
+    * @param {pros::Controller} joystick - this is the joystick that the function will retrieve button states and channel inputs from.
+    * @param {RAJA::ChassisObject} drive - this is the ChassisObject that the function will control.
+    */
+    void arcade_drive(pros::Controller &joystick, RAJA::ChassisObject &drive); 
+}
 
 #endif
--- src/LIBRARY/Drive-Opcontrol.cpp
+++ src/LIBRARY/Drive-Opcontrol.cpp
@@ -2,29 +2,33 @@
 #include <iostream>
 #include "../../include/RAJA/usercontrol/Drive-Opcontrol.h"
 
-void tank_drive(pros::Controller &joystick, RAJA::ChassisObject &drive){
-    int right_channel = joystick.get_analog(ANALOG_RIGHT_Y); 
-    int left_channel = joystick.get_analog(ANALOG_LEFT_Y); 
+namespace RAJA{
+    
+    void tank_drive(pros::Controller &joystick, RAJA::ChassisObject &drive){
+        int right_channel = joystick.get_analog(ANALOG_RIGHT_Y); 
+        int left_channel = joystick.get_analog(ANALOG_LEFT_Y); 
 
-    if(abs(right_channel) > STANDARD_DEADBAND_ERROR){
-        drive.move_right_side(right_channel); 
-    } 
-    else drive.move_right_side(0); 
+        if(abs(right_channel) > STANDARD_DEADBAND_ERROR){
+            drive.move_right_side(right_channel); 
+        } 
+        else drive.move_right_side(0); 
 
-    if(abs(left_channel) > STANDARD_DEADBAND_ERROR){
-        drive.move_left_side(left_channel); 
-    } 
-    else drive.move_left_side(0); 
-}
+        if(abs(left_channel) > STANDARD_DEADBAND_ERROR){
+            drive.move_left_side(left_channel); 
+        } 
+        else drive.move_left_side(0); 
+    }
 
-void arcade_drive(pros::Controller &joystick, RAJA::ChassisObject &drive){
-    int right_channel = joystick.get_analog(ANALOG_RIGHT_Y);
-    int left_x_channel = joystick.get_analog(ANALOG_LEFT_X); 
+    void arcade_drive(pros::Controller &joystick, RAJA::ChassisObject &drive){
+        int right_channel = joystick.get_analog(ANALOG_RIGHT_Y);
+        int left_x_channel = joystick.get_analog(ANALOG_LEFT_X); 
 
-    int linear_power = (abs(right_channel) > STANDARD_DEADBAND_ERROR) ? right_channel : 0; 
-    int turn_power = (abs(left_x_channel) > STANDARD_DEADBAND_ERROR) ? left_x_channel : 0; 
+        int linear_power = (abs(right_channel) > STANDARD_DEADBAND_ERROR) ? right_channel : 0; 
+        int turn_power = (abs(left_x_channel) > STANDARD_DEADBAND_ERROR) ? left_x_channel : 0; 
 
-    drive.move_left_side(linear_power + turn_power);
-    drive.move_right_side(linear_power - turn_power);
+        drive.move_left_side(linear_power + turn_power);
+        drive.move_right_side(linear_power - turn_power);
+
+    }
 
 }
