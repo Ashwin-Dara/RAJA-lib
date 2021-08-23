52bce845a1c587b12036c5694e3e4de8fd4a5139
Ashwin D <niwhsa.d@gmail.com>
2021-08-23 16:20:35 -0700

    Finished refactoring the rising edge header file. Need to add it to the RAJA namespace.

--- include/RAJA/usercontrol/Rising-Edge.h
+++ include/RAJA/usercontrol/Rising-Edge.h
@@ -1,15 +1,25 @@
 #ifndef RISING_EDGE_H
 #define RISING_EDGE_H
+
 #include "main.h"
 
-bool rising_edge(pros::Controller &joytick, pros::controller_digital_e_t button);
-// USAGE 
-/* 
+namespace RAJA{
+    
+    /*
+    * Rising Edge Controller. I.e., if a button is held down, the rising_edge function will return true only for one cycle. 
+    *
+    * @param {pros::Controller} joystick - controller that you wish to use
+    * @param {pros::controller_digital_e_t} button - this is the button enum that you wish to retrieve the rising edge state for
+    */
+    bool rising_edge(pros::Controller &joystick, pros::controller_digital_e_t button);
 
-    if(rising_edge(joystick, E_CONROLLER_DIGITAL_R1)){
-        
-    }
-
-*/
+    /*
+    * Example usage. 
+    *
+    * 1| pros::Controller controller1(E_MASTER_CONTROLLER);
+    * 2| if(rising_edge(controller1, DIGITAL_L1); 
+    * 3| // If the button is held down, it will still only return true once. Very useful for counts. 
+    */
+}
 
 #endif
--- src/LIBRARY/Rising-Edge.cpp
+++ src/LIBRARY/Rising-Edge.cpp
@@ -4,7 +4,7 @@
 bool rising_edge(pros::Controller &joystick, pros::controller_digital_e_t button){
     bool current_state = joystick.get_digital(button);
     pros::delay(20);
-    return (bool) (!current_state && joystick.get_digital(button));
+    return (bool) (!current_state && joystick.get_digital(button)); // Only return otrue if the button in the previous cycle was false and the button is being pressed. 
 }
 
 // TESTING ENSURING USAGE PRODUCES NO SYNTAX ERRORS 
