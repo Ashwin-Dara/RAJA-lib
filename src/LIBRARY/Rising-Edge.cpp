1206a530515d71b5207335d4c890b834bd01a11b
Ashwin D <niwhsa.d@gmail.com>
2021-08-23 16:22:37 -0700

    Added the rising edge to the RAJA namespace.

--- src/LIBRARY/Rising-Edge.cpp
+++ src/LIBRARY/Rising-Edge.cpp
@@ -1,14 +1,11 @@
 #include "main.h"
 #include "../../include/RAJA/usercontrol/Rising-Edge.h"
 
-bool rising_edge(pros::Controller &joystick, pros::controller_digital_e_t button){
-    bool current_state = joystick.get_digital(button);
-    pros::delay(20);
-    return (bool) (!current_state && joystick.get_digital(button)); // Only return otrue if the button in the previous cycle was false and the button is being pressed. 
-}
+namespace RAJA{
 
-// TESTING ENSURING USAGE PRODUCES NO SYNTAX ERRORS 
-/* 
-pros::Controller joystick(pros::E_CONTROLLER_MASTER);
-bool state = rising_edge(joystick, pros::E_CONTROLLER_DIGITAL_L2); 
-*/
+    bool rising_edge(pros::Controller &joystick, pros::controller_digital_e_t button){
+        bool current_state = joystick.get_digital(button);
+        pros::delay(20);
+        return (bool) (!current_state && joystick.get_digital(button)); // Only return otrue if the button in the previous cycle was false and the button is being pressed. 
+    }
+}
